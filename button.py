e44e30c9d67f8f715373536092fe2237aec21020
danimtb <danimanzaneque@gmail.com>
2017-07-10 14:03:11 +0200

    Removed unnecessary lines

--- button.py
+++ button.py
@@ -1,16 +1,7 @@
 #!/usr/bin/env python3
 
-# http://www.raspberrypi-spy.co.uk/2013/07/running-a-python-script-at-boot-using-cron/
-# crontab -e
-# @reboot   python /PATH/TO/REPO/button.py
-
-# Exit daemon:
-# find number: ps aux | grep "button.py"
-# kill process: sudo kill 61604
-
 import json
 import logging
-logging.getLogger("scapy.runtime").setLevel(logging.ERROR)
 from scapy.all import sniff
 from scapy.all import ARP
 import requests
@@ -25,7 +16,7 @@ def arp_display(pkt):
 
         logging.info(button['name'])
         r = requests.post(button['url'], json=button['body'], headers=button['headers'])
-        logging.info('status code: {}'.format(r.status_code))
+        logging.info('Status Code: {}'.format(r.status_code))
 
 
 # create basepath
