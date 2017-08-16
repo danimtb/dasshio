7655db9b8d6ba250bc09b7b88978561b5a6a5cd6
danimtb <danimanzaneque@gmail.com>
2017-08-16 17:04:08 +0200

    Added Guard to avoid multiple requests on a single press

--- dasshio/dasshio.py
+++ dasshio/dasshio.py
@@ -7,29 +7,34 @@ import requests
 from scapy.all import sniff
 from scapy.all import ARP
 import sys
-import time
 
 
 def arp_display(pkt):
     mac = pkt[ARP].hwsrc.lower()
-    if mac in [button['address'].lower() for button in config['buttons']]:
-        idx = [button['address'].lower() for button in config['buttons']].index(mac)
-        button = config['buttons'][idx]
 
-        logging.info(button['name'] + " button pressed!")
-        logging.info("Request: " + button['url'])
-        
-        try:
-            request = requests.post(button['url'], json=json.loads(button['body']), headers=json.loads(button['headers']))
-            logging.info('Status Code: {}'.format(request.status_code))
+    for button in config['buttons']:
+        if mac == button['address'].lower() and guard[button['address']] == False:
+
+            guard[button['address']] = True
+
+            idx = [button['address'].lower() for button in config['buttons']].index(mac)
+            button = config['buttons'][idx]
+
+            logging.info(button['name'] + " button pressed!")
+            logging.info("Request: " + button['url'])
             
-            if request.status_code == requests.codes.ok:
-                logging.info("Successful request")
-                time.sleep(5) # Wait 5 seconds to let dash button disconnect from wifi before scanning again
-            else:
-                logging.error("Bad request")
-        except:
-            logging.exception("Unable to perform  request: Check url, body and headers format. Check API password")
+            try:
+                request = requests.post(button['url'], json=json.loads(button['body']), headers=json.loads(button['headers']))
+                logging.info('Status Code: {}'.format(request.status_code))
+                
+                if request.status_code == requests.codes.ok:
+                    logging.info("Successful request")
+                else:
+                    logging.error("Bad request")
+            except:
+                logging.exception("Unable to perform  request: Check url, body and headers format. Check API password")
+            finally:
+                guard[button['address']] = False
 
 
 # Create basepath
@@ -53,6 +58,12 @@ logging.info("Reading config file: /data/options.json")
 
 with open(path + '/data/options.json', mode='r') as data_file:
     config = json.load(data_file)
+
+#Create Guard dict to avoid multiple requests with a single press and set to false for all buttons
+guard = dict()
+
+for button in config['buttons']:
+    guard[button['address']] = False
 
 # Start sniffing
 logging.info("Starting sniffing...")
