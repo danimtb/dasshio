11f856d8d17fc313df9e150df13fe2906a1e61ba
PaulFinch <p4ulf1nch@gmail.com>
2017-11-11 21:23:49 +0100

    Add Check config parameters

--- dasshio/dasshio.py
+++ dasshio/dasshio.py
@@ -13,6 +13,7 @@ from scapy.all import Ether
 import sys
 import time
 import signal
+import re
 
 def signal_handler(signal, frame):
     sys.exit(0)
@@ -75,6 +76,25 @@ logging.info("Reading config file: /data/options.json")
 with open(path + '/data/options.json', mode='r') as data_file:
     config = json.load(data_file)
 
+# Check config parameters
+cpt = 0
+error = 0
+for button in config['buttons']:
+    cpt = cpt + 1
+    if ('address' not in button) or (not button['address']) or (not re.match("[0-9a-f]{2}([-:])[0-9a-f]{2}(\\1[0-9a-f]{2}){4}$", button['address'].lower())):
+        logging.info("Parameter error for button " + str(cpt) + " : address is not valid")
+        error = error + 1
+    for value in ('name', 'url'):
+        if (value not in button) or (not button[value]) or (button[value] == 'null'):
+            logging.info("Parameter error for button " + str(cpt) + " : " + value + " is null")
+            error = error + 1
+    for value in ('body', 'headers'):
+        if (value not in button) or (not button[value]):
+            button[value] = '{}'
+if error != 0:
+    logging.info("Exiting ...")
+    sys.exit(0)
+    
 while True:
     # Start sniffing
     logging.info("Starting sniffing...")
