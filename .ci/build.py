85a3362b46c0b1d0a6d6b92b1d7d31da05a0c712
danimtb <danimanzaneque@gmail.com>
2019-04-14 17:00:58 +0200

    fix docker login

--- .ci/build.py
+++ .ci/build.py
@@ -10,15 +10,10 @@ def run(cmd, message=None):
         raise Exception("Error running: %s" % msg)
 
 
-DOCKER_LOGIN = "docker login -u {docker_user} -p {docker_pass}"
 DOCKER_BUILD = "docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/{addon}:/data homeassistant/{arch}-builder -t /data --no-cache"
 
 addon = os.getenv("ADDON")
-docker_user = os.getenv("DOCKER_USER")
-docker_pass = os.getenv("DOCKER_PASS")
 
-
-run(DOCKER_LOGIN.format(docker_user=docker_user, docker_pass=docker_pass), "docker login")
 
 for arch in ["armhf", "amd64", "aarch64", "i386"]:
     run(DOCKER_BUILD.format(addon=addon, arch=arch))
