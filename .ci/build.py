4ee8b33e572906c74f2548cb6f419696c3306b3b
danimtb <danimanzaneque@gmail.com>
2019-04-14 17:17:28 +0200

    Use --all and --test in builder

--- .ci/build.py
+++ .ci/build.py
@@ -9,7 +9,10 @@ def run(cmd, message=None):
     if ret != 0:
         raise Exception("Error running: %s" % msg)
 
-
+travis_tag = os.getenv("TRAVIS_TAG")
 addon = os.getenv("ADDON")
-docker_build = "docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/{addon}:/data homeassistant/amd64-builder -t /data --no-cache"
+
+docker_build = "docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/{addon}:/data homeassistant/amd64-builder --all -t /data --no-cache"
+if not travis_tag:
+    docker_build = docker_build + " --test"
 run(docker_build.format(addon=addon))
