c33d2184800b57be9f68d0ed066dd81f7e1ec0e9
danimtb <danimanzaneque@gmail.com>
2019-11-15 23:39:01 +0100

    update docker command

--- .ci/build.py
+++ .ci/build.py
@@ -10,9 +10,21 @@ def run(cmd, message=None):
         raise Exception("Error running: %s" % msg)
 
 travis_tag = os.getenv("TRAVIS_TAG")
+travis_commmit = os.getenv("TRAVIS_COMMIT")
+github_url = os.getenv("GITHUB_URL")
 addon = os.getenv("ADDON")
 
-docker_build = "docker run --rm --privileged -v ~/.docker:/root/.docker -v $(pwd)/{addon}:/data homeassistant/amd64-builder --all -t /data --no-cache"
+docker_build = "docker run -it --rm --privileged --name {addon} " \
+               "-v ~/.docker:/root/.docker " \
+               "-v $(pwd)/{addon}:/data " \
+               "homeassistant/build-env:latest " \
+               "--target {addon} " \
+               "--tag-latest " \
+               "--all -t /data --no-cache " \
+               "--author 'Daniel Manzaneque <danimanzaneque@gmail.com>' " \
+               " --doc-url {github_url} " \
+               "--parallel " \
+               "--arg COMMIT {travis_commmit}'"
 if not travis_tag:
     docker_build = docker_build + " --test"
-run(docker_build.format(addon=addon))
+run(docker_build.format(addon=addon, travis_commmit=travis_commmit, github_url=github_url))
